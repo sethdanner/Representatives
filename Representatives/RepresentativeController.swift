e3f53b63a134450d13c68f497bf129b06f417a94
Seth <sethdanner@gmail.com>
2018-08-10 14:19:06 -0600

    Added properties and function to RepresentativeController.

--- Representatives/RepresentativeController.swift
+++ Representatives/RepresentativeController.swift
@@ -11,6 +11,15 @@ import Foundation
 class RepresentativeController {
     
     let representatives: [Representative] = []
+    static let baseURL = URL(string: "https://whoismyrepresentative.com/getall_reps_bystate.php")!
     
+    func searchRepresentatives(forState state: String, completion: @escaping ([Representative]) -> Void) {
+        
+        let url = RepresentativeController.baseURL
+        let urlParameters = ["state": "\(state)", "output": "json"]
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: true)
+        let queryItems = urlParameters.compactMap({ URLQueryItem(name: $0.key, value: $0.value) })
+        components?.queryItems = queryItems
+    }
     
 }
