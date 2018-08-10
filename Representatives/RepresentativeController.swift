a8b5cdf6138766777d4611ccef85998aab63528f
Seth <sethdanner@gmail.com>
2018-08-10 12:35:35 -0600

    Added States file and model controller.

---
+++ Representatives/RepresentativeController.swift
@@ -0,0 +1,17 @@
+//
+//  RepresentativeController.swift
+//  Representatives
+//
+//  Created by Seth Danner on 8/10/18.
+//  Copyright © 2018 Seth Danner. All rights reserved.
+//
+
+import Foundation
+
+class RepresentativeController {
+    
+    let representatives: [Representative] = []
+    let shared = RepresentativeController()
+    
+    
+}
