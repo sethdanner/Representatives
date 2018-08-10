c38f0372bb1238bdbb10efb5805c44a0716e8e64
Seth <sethdanner@gmail.com>
2018-08-10 11:58:47 -0600

    Added Representative model.

---
+++ Representatives/Representative.swift
@@ -0,0 +1,20 @@
+//
+//  Representative.swift
+//  Representatives
+//
+//  Created by Seth Danner on 8/10/18.
+//  Copyright © 2018 Seth Danner. All rights reserved.
+//
+
+import Foundation
+
+struct Representative: Codable {
+    
+    let name: String
+    let party: String
+    let state: String
+    let district: String
+    let phone: String
+    let office: String
+    let link: String
+}
